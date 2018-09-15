b0e19d62732d71db5964ee1f3b34a09d842113fa
tracer-ics2018 <tracer@njuics.org>
2018-09-15 07:33:02 +0000

    >  compile 171240565 ics Linux fb2bea201877 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  07:33:02 up  5:03,  3 users,  load average: 0.01, 0.02, 0.00 1cf9e178a4f195fefa57c4d3f8f83767d1c42745

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -99,9 +99,11 @@ printf("%x",ad);*/
 sscanf(args,"%u 0x%x",&l,&ad);
 printf("%u",l);
 printf("\n0x%x",ad);
+uint8_t temp;
 for(i=0;i<l;i++)
 {/*if(!(i&0xf))*/printf("\n0x%08x:",(ad+i*16));
-
+temp=pmem[ad+i];
+printf("0x%08x",(unsigned int)temp);
 	//	printf("0x%02x",*(unsigned char *)hwa_to_va((ad+i)));
 		}
 printf("\n");

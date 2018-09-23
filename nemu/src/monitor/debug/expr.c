46f58f7c75636c15e614e25eaa6ea3831c005b55
tracer-ics2018 <tracer@njuics.org>
2018-09-23 18:07:53 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:07:53 up  4:24,  3 users,  load average: 0.13, 0.17, 0.08 bcc03131c20c4e89fc34393eeab9ffb9b0fc5a04

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,11 +1,13 @@
 #include "nemu.h"
+#define COLORRED " \033[1;33m"
+#define COLORNORMAL "\033[0m"
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <sys/types.h>
 #include <regex.h>
-
+#include <stdlib.h>
 enum {
   TK_NOTYPE = 256, TK_EQ,NAT_NUM,NEG
 
@@ -69,7 +71,7 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
+  while (e[position] != '\2') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
@@ -96,7 +98,8 @@ static bool make_token(char *e) {
 			case '-':if(nr_token==0)
 					 {tokens[nr_token].type=NEG;}
 					 else {tokens[nr_token].type=rules[i].token_type;
-					 }				 
+					 }
+
 	   		default: tokens[nr_token].type=rules[i].token_type;
 				   break;
         }
@@ -114,6 +117,100 @@ static bool make_token(char *e) {
   }
 
   return true;
+}
+//begin
+bool check_parentheses(int p,int q){
+	int i,par=0;
+	if(tokens[p].type!='('||tokens[q].type!=')')
+	return false;
+	for(i=p;i<q;++i){
+		if(tokens[i].type=='(')
+		par++;
+		else if(tokens[i].type==')')
+		par--;
+		if(par==0&&i<q)
+		return false;
+}
+	return true;
+	}
+  
+int priority(int i){
+switch (i){
+	case (int)'+':return 5;
+	case (int)'-':return 5;
+	case (int)'*':return 2;
+	case (int)'/':return 2;
+	default:return -1;
+}
+}
+  
+int dominant_operator_place(int p,int q){
+	int i;
+	int pri,dom;
+	dom=-1;
+	pri=20;
+	int min=20;
+	for( i=p;i<=q;++i){
+		if(tokens[i].type=='('){
+			i++;
+			while(tokens[i].type!=')')
+				i++;
+			i++;
+			if(i>q)
+				break;
+		}
+//	tp=tokens[i].type;
+	//wait to continue
+	pri=priority(i);
+	if(pri<min&&pri!=-1)
+	{	min=pri;
+		dom=i;
+		}
+	}
+	if(pri==-1){
+	printf(COLORRED "The operator can't be found here.\n" COLORNORMAL);
+	assert(0);
+	}
+	return dom;
+}
+	
+uint32_t eval(int p,int q){
+	if(p>q){
+		printf(COLORRED "Bad expression\n" COLORNORMAL);
+		return false;
+	}
+	else if(p==q){
+		int type=tokens[p].type;
+//	uint32_t t=0;
+
+	switch(type){
+		case NAT_NUM:return atoi(tokens[p].str);
+
+		default:assert(0);
+	}
+	}
+	else if(check_parentheses(p,q)==true){
+
+		return eval(p+1,q-1);
+			}
+	 else {
+int op=dominant_operator_place(p,q) ;
+		
+
+
+		int val1=eval(p,op-1);
+		int val2=eval(op+1,q);
+		switch(tokens[op].type){
+			case '+':return val1+val2;
+			case '-':return val1-val2;
+			case '*':return val1*val2;
+			case '/':return val1/val2;
+
+
+
+			default:assert(0);
+			}
+		}
 }
 
 uint32_t expr(char *e, bool *success) {

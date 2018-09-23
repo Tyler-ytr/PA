f38eff11556e196deaac4c9a5fcc8fa78c165885
tracer-ics2018 <tracer@njuics.org>
2018-09-23 20:10:52 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:10:52 up 50 min,  5 users,  load average: 1.05, 1.00, 0.59 6ff32dde41e22722df7e492d3823aed124f2bee6

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -112,12 +112,13 @@ printf("%x",ad);*/
 }
 static int cmd_p(char *args)
 {
-	
+	printf("in it!");
 	bool SUCCESS=0;
 	bool *Success=NULL;
 	Success = &SUCCESS;
 	char *E=strtok(NULL," ");
 	uint32_t result=expr(E,Success);
+	printf("%s",E);
 	if(*Success==false)
 	{	printf("\033[1;33m""Invaild input.\n""\033[0m");
 		return 1;

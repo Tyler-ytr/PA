9910f8a7bcf7106d2aaf50123d8edf9c99b4ef71
tracer-ics2018 <tracer@njuics.org>
2018-09-27 00:37:18 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:37:18 up  4:23,  3 users,  load average: 0.08, 0.05, 0.01 eb17e3a0676203f0c7c72cb1409180ccac5ab39

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,10 +1,32 @@
+
+//#include"nemu.h"
+//#include<main.h>
+#include"monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+	int is_batch_mode=init_monitor(argc,argv);
+ uint32_t tresult[1000];
+ char tbuf[65535];
+ bool tsuccess;
+ uint32_t ttresult;
+ bool*ts=&tsuccess;
+//   	int is_batch_mode = init_monitor(argc, argv);
+FILE *ffp=fopen("..//tools/gen-expr/input","r");
+//while(~fscanf){int i=0;
+int i=0;	
+while(fscanf(ffp,"%u%s%*[^\n]%*c",&tresult[i],tbuf)){
 
+ttresult=expr(tbuf,ts);
+printf("trueresult:%d testresult: %d",tresult[i],ttresult);
+if(ttresult!=tresult[i]){
+printf("wrong result of %s\n",tbuf);
+break;
+}i++;
+}
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -10,14 +10,14 @@ static char buf[65536];
 
 
 static inline uint32_t choose(int n)
-{
-	srand((unsigned)time(NULL));
-	uint32_t i=(uint32_t)(rand()%(n));
+{    
+//	srand((unsigned)time(NULL));
+	int i=(uint32_t)(rand()%(n));
 	return i;
 }
 static inline uint32_t chooseoff(int n)
 {
-	srand((unsigned)time(NULL));
+//	srand((unsigned)time(NULL));
 	uint32_t i=(uint32_t)(rand()%n)+1;
 	return i;
 }
@@ -25,59 +25,72 @@ static inline uint32_t chooseoff(int n)
 
 
 static inline void gen_num(int *p)
-{
-	uint32_t ran=choose(32767);
-	if(ran==0&&buf[*p-1]=='/')
-	{
-	ran=chooseoff(32767);
+{   int c=0;
+	uint32_t lens=choose(10)+1;
+	char find[10]={'0','1','2','3','4','5','6','7','8','9'};
+	int flag=0;
+	if(lens==1)flag=1;
+	for(int i=0;i<lens;i++)
+	{	
+	c=choose(10);
+	if(c==0&&(*p==0||buf[*p-1]=='+'||buf[*p-1]=='-'||buf[*p-1]=='*'||buf[*p-1]=='/'||buf[*p-1]=='(')){
+	
+	continue;
 	}
-	buf[*p]=ran;
-	buf[*p+1]='\0';
+	buf[*p]=find[c];
 	*p=*p+1;
+	buf[*p]='\0';
+	}	
+
+
+
+//	buf[*p]=ran;
+//	buf[*p+1]='\0';
+
 }
 static inline void gen_rand_op(int *p)
 {
-	int k=choose(4);
+	unsigned int k=rand()%3;
 	switch(k)
 	{
-		case 0:buf[*p]='+';
-		case 1:buf[*p]='-';
-		case 2:buf[*p]='*';
-		case 3:buf[*p]='/';
-		default :assert(0);
+	//	case 0:buf[*p]='+';
+		case 1:buf[*p]='-';break;
+		case 2:buf[*p]='*';break;
+		case 3:buf[*p]='/';break;
+		default:buf[*p]='+';break;
 	}	
 	buf[*p+1]='\0';
 	*p=*p+1;
 
 }
-static inline void gen(char p,int *p)
+static inline void gen(char t,int *p)
 {
-	buf[*p]=p;
+	buf[*p]=t;
 	*p=*p+1;
 }
 
 
 
-static inline void gen_rand_expr() {
+static inline void gen_rand_expr(int *P) {
  // buf[0] = '\0';
- int place=0;
- int *P=&place;
- if(place<65500)
+// int place=0;
+// int *P=&place;
+
   switch(choose(3))
   {
 	case 0:gen_num(P);break;
-	case 1:gen('(',P);gen_rand_expr();gen(')',P);break;
-	default :gen_rand_expr();gen_rand_op(P);gen_rand_expr();break;  
+	case 1:gen('(',P);gen_rand_expr(P);gen(')',P);break;
+	default :gen_rand_expr(P);gen_rand_op(P);gen_rand_expr(P);break;  
   
   }
- else return;
+ 
 }
 
 static char code_buf[65536];
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
+"  unsigned result=%s; "
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
@@ -90,17 +103,20 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
+  int place=0;
+  int *p=&place;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+	if(place<50000)
+  	  gen_rand_expr(p);
 
-    sprintf(code_buf, code_format, buf);
+    sprintf(code_buf, code_format, buf);//insert buf into code_buf
 
-    FILE *fp = fopen(".code.c", "w");
+    FILE *fp = fopen(".code.c", "w");//creat c.code for writing in
     assert(fp != NULL);
     fputs(code_buf, fp);
-    fclose(fp);
+    fclose(fp);//close the fp
 
-    int ret = system("gcc .code.c -o .expr");
+    int ret = system("gcc .code.c -o .expr");//call the system to create .expr based on code.c
     if (ret != 0) continue;
 
     fp = popen("./.expr", "r");

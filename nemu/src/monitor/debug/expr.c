208cd8b11ec8b07a74af8abd73c49099f50ce720
tracer-ics2018 <tracer@njuics.org>
2018-09-27 15:33:51 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:33:51 up  1:30,  3 users,  load average: 0.23, 0.29, 0.21 bf44ca7f43a3207984779e4ae254372f9a379aaa

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -123,7 +123,7 @@ static bool make_token(char *e) {
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
+    assert(0);  return false;
     }
   }
 

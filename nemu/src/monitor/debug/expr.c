6980df4d5d38ab002425fe22fa77f62ff29aeda3
tracer-ics2018 <tracer@njuics.org>
2018-09-27 16:04:26 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:04:26 up  1:54,  3 users,  load average: 0.12, 0.25, 0.19 9b8478c22c608a442607b52adfd607a4502199

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -92,7 +92,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-printf("%d\n",position);
+//printf("%d\n",position);
         switch (rules[i].token_type) {
 			case TK_NOTYPE: break;
 			case NAT_NUM:
@@ -111,7 +111,8 @@ printf("%d\n",position);
 	   		default: tokens[nr_token].type=rules[i].token_type;
 					 nr_token++;
 				   break;
-        }
+    }
+		assert(0);
 // printf("%d%s",tokens[nr_token].type,tokens[nr_token].str);   
         break;
       }

df21201c7f48ccf86c6545442fe0f444a34b7df6
tracer-ics2018 <tracer@njuics.org>
2018-09-27 15:27:49 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:27:49 up  1:24,  3 users,  load average: 0.33, 0.29, 0.19 e5a71a2a9ae8ec797b23c5c3f2a44e8580df9a05

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -71,7 +71,7 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
-
+assert(0);
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
@@ -194,7 +194,7 @@ uint32_t getOp(uint32_t p,uint32_t q)
 uint32_t eval(int p,int q){
 //plus
 //printf("orignal p=%d,q=%d",p,q);
-assert(0);
+
 	if(p>q){
 		printf("overflow:p>q,p=%d,q=%d\n",p,q);
 		assert(0);

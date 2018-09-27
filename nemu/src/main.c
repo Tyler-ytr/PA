0bc84865c4c923101f2e221b46b30d08c0ae6d61
tracer-ics2018 <tracer@njuics.org>
2018-09-27 10:32:56 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:32:56 up 24 min,  3 users,  load average: 0.15, 0.15, 0.19 c75dc2a6fafbac8c911d2020b026c9364cf921e4

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[]) {
 // bool*ts=&tsuccess;
 //   	int is_batch_mode = init_monitor(argc, argv);
 FILE *ffp=fopen("..//tools/gen-expr/input","r");
-fscanf(ffp,"%u%*[^\n]%*c%s",&tresult,ttemp);
+fscanf(ffp,"%u",&tresult);
+fscanf(ffp,"%s",ttemp);
+//fscanf(ffp,"%u%*[^\n]%*c%s",&tresult,ttemp);
 //while(~fscanf){int i=0;
 /*	
 while(fgets(ttemp,65535,ffp)){

b0643d87e62844acdab5bbb27bdaf1acbdf5202e
tracer-ics2018 <tracer@njuics.org>
2018-09-27 16:21:23 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:21:23 up  2:11,  3 users,  load average: 0.55, 0.22, 0.15 64d87f2bd2867decf88d2730eaa16e7a4ca6996f

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -51,34 +51,8 @@ break;
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 	int is_batch_mode=init_monitor(argc,argv);
-//	assert(0);
-	woqu();
-//	uint32_t expr(char *e,bool*success);
-// uint32_t tresult;
-// char tbuf[65535];
-// char ttemp[65536];
- //bool tsuccess;
-// uint32_t ttresult;
-// bool*ts=&tsuccess;
-//   	int is_batch_mode = init_monitor(argc, argv);
-//FILE *ffp=fopen("..//tools/gen-expr/input","r");
-//fscanf(ffp,"%u",&tresult);
-//printf("yingyingying");
-//fscanf(ffp,"%s",ttemp);
-//fscanf(ffp,"%u%*[^\n]%*c%s",&tresult,ttemp);
-//while(~fscanf){int i=0;
-/*	
-while(fgets(ttemp,65535,ffp)){
-char* cresult=strtok(ttemp," ");
-sscanf(cresult,"%d",&tresult);
-ttresult=expr(tbuf,ts);
-printf("trueresult:%d testresult: %d",tresult,ttresult);
-if(ttresult!=tresult){
-printf("wrong result of %s\n",tbuf);
-break;
-}
 
-}*/
+	woqu();
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 

2ff13cd07eca4afb60aedd3e8bea62b660b7dd63
tracer-ics2018 <tracer@njuics.org>
2018-09-27 10:30:37 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:30:37 up 21 min,  3 users,  load average: 0.33, 0.18, 0.22 71629bf2a16ba1487874aaef404c833927723a3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,16 +11,16 @@ int main(int argc, char *argv[]) {
 	int is_batch_mode=init_monitor(argc,argv);
 	uint32_t expr(char *e,bool*success);
  uint32_t tresult;
- char tbuf[65535];
+// char tbuf[65535];
  char ttemp[65536];
- bool tsuccess;
- uint32_t ttresult;
- bool*ts=&tsuccess;
+ //bool tsuccess;
+// uint32_t ttresult;
+// bool*ts=&tsuccess;
 //   	int is_batch_mode = init_monitor(argc, argv);
 FILE *ffp=fopen("..//tools/gen-expr/input","r");
 fscanf(ffp,"%u%*[^\n]%*c%s",&tresult,ttemp);
 //while(~fscanf){int i=0;
-	
+/*	
 while(fgets(ttemp,65535,ffp)){
 char* cresult=strtok(ttemp," ");
 sscanf(cresult,"%d",&tresult);
@@ -31,7 +31,7 @@ printf("wrong result of %s\n",tbuf);
 break;
 }
 
-}
+}*/
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 

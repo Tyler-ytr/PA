21763d13ca438f78e0dc8a64778b12aed6a1261b
tracer-ics2018 <tracer@njuics.org>
2018-10-05 16:22:14 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:22:14 up 14:59,  9 users,  load average: 0.33, 0.28, 0.17 eeb9228e15126223872824a6f81325fe46fe1959

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -14,7 +14,13 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
+//  TODO();
+	rtl_xor(&t2,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t2);
+	rtl_update_ZFSF(&t2,id_dest->width);
+	t0=0;
+	rtl_set_CF(&t0);
+	rtl_set_OF(&t0);
 
   print_asm_template2(xor);
 }

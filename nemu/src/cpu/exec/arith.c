fdc613fdc868b3b87eeb96b48fa1673fa5ba0d42
tracer-ics2018 <tracer@njuics.org>
2018-10-05 15:55:42 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:55:42 up 14:33,  7 users,  load average: 0.12, 0.17, 0.09 f65102229fa0d171e64fc9772ab4ff1ffe6f51a2

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -9,7 +9,24 @@ make_EHelper(add) {
 make_EHelper(sub) {
 //  TODO();
 //printf("here miaomiaomiao\n");
-rtl_sub(&t1,&id_dest->val,&id_src->val);
+	rtl_sub(&t2,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t2);
+
+    rtl_update_ZFSF(&t2,id_dest->width);
+
+	rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
+	rtl_or(&t0, &t3, &t0);
+	rtl_set_CF(&t0);
+    
+	rtl_xor(&t0, &id_dest->val, &id_src->val);
+	rtl_not(&t0, &t0);
+	rtl_xor(&t1, &id_dest->val, &t2);
+	rtl_and(&t0, &t0, &t1);
+	rtl_msb(&t0, &t0, id_dest->width);
+	rtl_set_OF(&t0);
+	
+
+
   print_asm_template2(sub);
 }
 

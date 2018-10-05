b98086c6fb2e5803a21147c878f398b80fb8fa2b
tracer-ics2018 <tracer@njuics.org>
2018-10-05 16:05:24 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:05:24 up 14:42,  9 users,  load average: 0.37, 0.20, 0.14 bc61e18943a46d1ce2653e1e2c8b9a65aab5117f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -21,12 +21,12 @@ make_EHelper(sub) {
 	rtl_or(&t0, &t3, &t0);
 	rtl_set_CF(&t0);
     
-//	rtl_xor(&t0, &id_dest->val, &id_src->val);
-//	rtl_not(&t0, &t0);
-//	rtl_xor(&t1, &id_dest->val, &t2);
-//	rtl_and(&t0, &t0, &t1);
-//	rtl_msb(&t0, &t0, id_dest->width);
-//	rtl_set_OF(&t0);
+	rtl_xor(&t0, &id_dest->val, &id_src->val);
+	rtl_not(&t0, &t0);
+	rtl_xor(&t1, &id_dest->val, &t2);
+	rtl_and(&t0, &t0, &t1);
+	rtl_msb(&t0, &t0, id_dest->width);
+	rtl_set_OF(&t0);
 	
 
 

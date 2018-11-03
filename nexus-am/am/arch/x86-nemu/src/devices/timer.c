e1f967a4d2f10dc1ca64ad7c3260374ddb9d6f10
tracer-ics2018 <tracer@njuics.org>
2018-11-03 14:26:59 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:26:59 up 13:44, 13 users,  load average: 0.25, 0.13, 0.10 81fbdee2e0c334c0696a89e3a2da3a5e2e087f3c

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -1,13 +1,14 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
-
+extern void timer_init();
+_UptimeReg rtc_start;
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      uptime->hi = (inl(0x48))<<5;
-      uptime->lo = inl(0x48);
+      uptime->hi = 0;
+      uptime->lo = inl(0x48)-rtc_start.lo;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
@@ -23,6 +24,8 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   }
   return 0;
 }
-
+//RTC_PORT=0X48
 void timer_init() {
+rtc_start.hi=0;
+rtc_start.lo=inl(0x48);
 }

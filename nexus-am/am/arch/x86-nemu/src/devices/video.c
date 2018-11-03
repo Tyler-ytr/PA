8ab7de364582c954204b637eab831f18eeed1aa4
tracer-ics2018 <tracer@njuics.org>
2018-11-04 01:31:41 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:31:41 up 21:25, 15 users,  load average: 0.17, 0.50, 0.33 9983c25f4ace4fa83de6ae0396bcb97b1d09eaf

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -10,7 +10,9 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
       info->width = inw(0x100);
+      info->width =400;
       info->height =( inl(0x100)>>16);
+      info->height =300;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -26,7 +28,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 	  int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
 	  uint32_t *pixels=ctl->pixels;
 	  int k;
-	  if(w>400-x)
+	  if(w<400-x)
 		  k=w;
 	  else k=400-x;
 	  int cp_bytes=sizeof(uint32_t)*k;

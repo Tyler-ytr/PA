610a4f7d5f459fa6411b406a3c907a37cdf9eb00
tracer-ics2018 <tracer@njuics.org>
2018-11-03 11:10:07 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:10:07 up 13:00, 12 users,  load average: 0.38, 0.17, 0.11 6d66c1b85af2baa44596de8fdd995219b46fac3

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -165,7 +165,7 @@ make_DHelper(lea_M2G) {
  * eAX <- Iv
  */
 make_DHelper(I2a) {
-//Log("I am in I2a");
+Log("I am in I2a");
   	decode_op_a(eip, id_dest, true);
   decode_op_I(eip, id_src, true);
 }

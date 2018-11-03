e00abc9a1f95098fc75d9a5a39042c3e89e861ad
tracer-ics2018 <tracer@njuics.org>
2018-11-04 01:35:57 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:35:57 up 21:29, 15 users,  load average: 0.37, 0.48, 0.37 7d2b331c50e6804ba36cfeac416f10df816b00e

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -9,7 +9,7 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = inl(0x100)&0x00ff;
+      info->width = inw(0x100)<<16;
      // info->width =400;
       info->height =( inl(0x100)>>16);
      // info->height =300;

b74477a5b14985b61485d95bb73f9010674d0d4c
tracer-ics2018 <tracer@njuics.org>
2018-11-04 01:21:02 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:21:02 up 21:14, 15 users,  load average: 0.12, 0.10, 0.09 9873e2c06a02f51cf8ec7a6ab40bd6593191f7c3

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -21,11 +21,24 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-	  int i;
-	  int size=screen_width()*screen_height();
-	 ctl->w=ctl->x/inw(0x100);
-	ctl->h=ctl->y/(inl(0x100)>>16); 
-	  for(i=0;i<size;i++)fb[i]=*ctl->pixels;
+	  
+	//  int size=screen_width()*screen_height();
+	  int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+	  uint32_t *pixels=ctl->pixels;
+	  int k;
+	  if(w<inw(0x100)-x)
+		  k=w;
+	  else k=inw(0x100)-x;
+	  int cp_bytes=sizeof(uint32_t)*k;
+	  for(int j=0;j<h&&y+j<(inl(0x100)>>16);j++)
+	  {
+		  memcpy(&fb[(y+j)*inw(0x100)+x],pixels,cp_bytes);
+		  
+		  pixels+=w;
+	  
+	  
+	  
+	  }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }

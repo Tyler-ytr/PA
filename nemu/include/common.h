9b02a75a1e0961eeae597ee5242145982de59596
tracer-ics2018 <tracer@njuics.org>
2018-11-11 23:39:44 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:39:44 up  6:20,  6 users,  load average: 0.48, 0.20, 0.12 50078b38cb4be4c228ba3401a80da2b74b9ec4b

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -2,7 +2,7 @@
 #define __COMMON_H__
 
 #define DEBUG
-#define DIFF_TEST
+//#define DIFF_TEST
 //volatile static int dummy=0;
 #if _SHARE
 // do not enable these features while building a reference design

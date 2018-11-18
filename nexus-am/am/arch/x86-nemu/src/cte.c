ae6a920eb0e2d8f1a7e684d746690c8694186da1
tracer-ics2018 <tracer@njuics.org>
2018-11-18 17:35:51 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:35:51 up  6:44, 19 users,  load average: 0.01, 0.07, 0.08 437c1722af22ebe6d93186dfc0a216e07792e7af

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -11,12 +11,14 @@ _Context* irq_handle(_Context *tf) {
 	_Context *next = tf;
 //	printf("tf:0x%x,eax:0x%x,ebx:0x%x,ecx:0x%x,edx:0x%x,esp:0x%x\n",tf,cpu.eax,cpu.ebx,cpu.ecx,cpu.edx,esp);
 	printf("tf:0x%x\n",tf);
+	printf("tf->irq:0x%x\n",tf->irq);
 //	assert(0);
   if (user_handler) {
     _Event ev;
 	
 	//assert(0);
     switch (tf->irq) {
+	  	
       default: ev.event = _EVENT_ERROR; break;
     }
 	

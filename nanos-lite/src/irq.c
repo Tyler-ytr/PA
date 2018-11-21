b64fb906f8dc7ba31c207a1d2bbd0aa82a9fed99
tracer-ics2018 <tracer@njuics.org>
2018-11-21 21:08:49 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:08:49 up  6:47, 27 users,  load average: 0.26, 0.10, 0.10 3a00286f513e77667d9a33465c7473a0a8813ca8

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,9 @@
 #include "common.h"
-
+extern _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
 	case _EVENT_YIELD:Log("I am in EVENT_YIELD event");break;
-case _EVENT_SYSCALL:Log("I am in SYSCALL event");break;
+case _EVENT_SYSCALL:return do_syscall(c);
     default: panic("Unhandled event ID = %d", e.event);
   }
 

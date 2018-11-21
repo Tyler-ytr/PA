86776e8de3d4c6e899c2f20068cb812894478b69
tracer-ics2018 <tracer@njuics.org>
2018-11-21 21:29:58 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:29:58 up  7:08, 29 users,  load average: 0.06, 0.07, 0.06 b978b29416459f892aafca3e31d02713f3d3710f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,9 +3,13 @@
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
-Log("I am in syscall");
+  a[0] = c->GPR1;//eax
+  a[1] = c->GPR2;//ebx
+  a[2] = c->GPR3;//ecx
+  a[3] = c->GPR4;//edx
+//Log("I am in syscall");
   switch (a[0]) {
+	  case SYS_yield:_yield();return 0; 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 

ab45bf3339276531997445d5f2f259990c099696
tracer-ics2018 <tracer@njuics.org>
2018-11-21 18:42:46 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:42:46 up  6:03, 25 users,  load average: 0.24, 0.32, 0.29 b37e26b7e11294443fc017f0881825161eb04698

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,7 +6,7 @@ extern uint8_t ramdisk_start;
 size_t ramdisk_write(const void *buf,size_t offset,size_t len);
 size_t ramdisk_read(void *buf,size_t offset,size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  //TODO();
 	size_t filelen=get_ramdisk_size();
 //char a[10000] ;
 //filelen=ramdisk_read(&a,0,filelen);

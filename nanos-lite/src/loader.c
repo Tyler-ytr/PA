b57c326e7fb387867ca911e68e65dc33a62c65f7
tracer-ics2018 <tracer@njuics.org>
2018-11-21 14:48:16 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:48:16 up  3:25, 17 users,  load average: 0.49, 0.38, 0.38 dc0b47052aecf36d5dc80c4042d188f2d9c305b6

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,9 +1,13 @@
 #include "proc.h"
-
+//#include "ramdisk.c"
 #define DEFAULT_ENTRY 0x4000000
+extern size_t get_ramdisk_size();
+size_t ramdisk_write(const void *buf,size_t offset,size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+//  TODO();a
+	size_t filelen=get_ramdisk_size();
+	filelen=ramdisk_write((void*)DEFAULT_ENTRY,0,filelen);
   return DEFAULT_ENTRY;
 }
 

382c60239a92525e25e2435f6e596aefab24d5ef
tracer-ics2018 <tracer@njuics.org>
2018-11-21 23:39:22 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:39:22 up  8:59, 33 users,  load average: 0.21, 0.20, 0.28 d19c98939aa0e35e208dcfed7d65018cd2b31b2e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,11 +7,11 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;//ebx
   a[2] = c->GPR3;//ecx
   a[3] = c->GPR4;//edx
-//Log("I am in syscall");
-//printf("a0(eax): 0x%x\n",a[0]);
-//printf("a0(ebx): 0x%x\n",a[1]);
-//printf("a0(ecx): 0x%x\n",a[2]);
-//printf("a0(edx): 0x%x\n",a[3]);
+Log("I am in syscall");
+printf("a0(eax): 0x%x\n",a[0]);
+printf("a0(ebx): 0x%x\n",a[1]);
+printf("a0(ecx): 0x%x\n",a[2]);
+printf("a0(edx): 0x%x\n",a[3]);
   switch (a[0]) {
 	  case SYS_exit:_halt(a[1]);					//ID=0
 	  case SYS_yield:_yield();return 0; //ID=1

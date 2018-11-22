3012876ba5615f5154eae798fb4f22213ab60d7b
tracer-ics2018 <tracer@njuics.org>
2018-11-22 11:45:47 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:45:47 up 11:23, 38 users,  load average: 0.22, 0.28, 0.30 842fca9d53cf9bf7945d53359a91d38e7a442c25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,7 +24,7 @@ printf("a1(ebx): 0x%x\n",a[1]);
 printf("a2(ecx): 0x%x\n",a[2]);
 printf("a3(edx): 0x%x\n",a[3]);
   switch (a[0]) {
-	  case SYS_exit:Log("I am before halt");_halt(a[1]);					//ID=0
+	  case SYS_exit:Log("I am before halt");_halt(a[0]);					//ID=0 it should be a[1] and the result of that is I can't go through the dummy. the result of it is ebx=0x1
 	  case SYS_yield:_yield();Log("I experienced yield");return 0; //ID=1
 	 
     default: panic("Unhandled syscall ID = %d", a[0]);

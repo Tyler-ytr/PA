901702baff77498e1ff6a78cb6b37321aaf65073
tracer-ics2018 <tracer@njuics.org>
2018-11-22 10:40:09 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:40:09 up 10:17, 35 users,  load average: 0.43, 0.47, 0.36 169608adeaf5b7ea5c3831462034c96edba41f2

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,19 @@
 #include "common.h"
 extern _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
+	Log("I am in do event");
+	      printf("c ->irq:0x%x\n",c->irq);                                                                                                                   
+	       printf(":0x%x\n",c);
+	        printf("c->edi:0x%x\n",c->edi);
+	         printf("c->esi:0x%x\n",c->esi);
+		      printf("c->ebp:0x%x\n",c->ebp);
+		       printf("c->esp:0x%x\n",c->esp);
+		       printf("c->edx:0x%x\n",c->edx);
+		         printf("c->ebx:0x%x\n",c->ebx);
+			      printf("c->ecx:0x%x\n",c->ecx);
+			       printf("c->eax:0x%x\n",c->eax);
+			       printf("\n");
+
   switch (e.event) {
 	case _EVENT_YIELD:Log("I am in EVENT_YIELD event");break;
 case _EVENT_SYSCALL:return do_syscall(c);
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,17 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;//ecx
   a[3] = c->GPR4;//edx
 Log("I am in syscall");
+    printf("c ->irq:0x%x\n",c->irq);                                                  
+	printf(":0x%x\n",c); 
+	printf("c->edi:0x%x\n",c->edi);                                               
+    printf("c->esi:0x%x\n",c->esi);                                       
+ 	printf("c->ebp:0x%x\n",c->ebp);             
+	printf("c->esp:0x%x\n",c->esp);								
+	printf("c->edx:0x%x\n",c->edx);                                                       
+	printf("c->ebx:0x%x\n",c->ebx);                                        
+	printf("c->ecx:0x%x\n",c->ecx);               
+	printf("c->eax:0x%x\n",c->eax); 
+   printf("\n");	
 printf("a0(eax): 0x%x\n",a[0]);
 printf("a1(ebx): 0x%x\n",a[1]);
 printf("a2(ecx): 0x%x\n",a[2]);

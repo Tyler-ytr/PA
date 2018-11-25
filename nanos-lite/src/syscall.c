6c475887726e70365815ede520b1e32357069a05
tracer-ics2018 <tracer@njuics.org>
2018-11-25 14:08:14 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:08:14 up 14:15, 33 users,  load average: 0.36, 0.23, 0.18 fbcacb74ec5c8d7647713f476016a000d23c6fcd

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,7 +8,7 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;//ebx
   a[2] = c->GPR3;//ecx
   a[3] = c->GPR4;//edx
-/*Log("I am in syscall");
+Log("I am in syscall");
     printf("c ->irq:0x%x\n",c->irq);                                                  
 	printf(":0x%x\n",c); 
 	printf("c->edi:0x%x\n",c->edi);                                               
@@ -23,7 +23,7 @@ _Context* do_syscall(_Context *c) {
 printf("a0(eax): 0x%x\n",a[0]);
 printf("a1(ebx): 0x%x\n",a[1]);
 printf("a2(ecx): 0x%x\n",a[2]);
-printf("a3(edx): 0x%x\n",a[3]);*/
+printf("a3(edx): 0x%x\n",a[3]);
   switch (a[0]) {
 	  case SYS_exit:_halt(a[1]);					//ID=0 it should be a[1] and the result of that is I can't go through the dummy. the result of it is ebx=0x1.it has fixed.
 	  case SYS_yield:{c->GPRx=sys_yield();
@@ -37,7 +37,9 @@ printf("a3(edx): 0x%x\n",a[3]);*/
 					 }
 		case SYS_open:Log("wait for sysopen");assert(0);
 		case SYS_read:Log("wait for sysread");assert(0);
-		case SYS_write:Log("wait for syswrite");assert(0);
+		case SYS_write:{
+					   
+					  Log("wait for syswrite");assert(0);}
     default: panic("Unhandled syscall ID = %d", a[0]);
   } 
 

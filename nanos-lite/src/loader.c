c8fb7a4f25b557500ae8205ae3d43111dac44090
tracer-ics2018 <tracer@njuics.org>
2018-12-01 00:34:48 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:34:48 up 13:38, 20 users,  load average: 0.62, 0.24, 0.09 afa54395824ef3be25f35f93e1bededc2605e6b9

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,24 +1,35 @@
 #include "proc.h"
+#include <sys/types.h>
 //#include "ramdisk.c"
 #define DEFAULT_ENTRY 0x4000000
 extern size_t get_ramdisk_size();
 extern uint8_t ramdisk_start;
-size_t ramdisk_write(const void *buf,size_t offset,size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len); 
+//size_t ramdisk_write(const void *buf,size_t offset,size_t len);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
 size_t ramdisk_read(void *buf,size_t offset,size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Log("I am in loader");
-	size_t filelen=get_ramdisk_size();
+//	size_t filelen=get_ramdisk_size();
 //char a[10000] ;
 //filelen=ramdisk_read(&a,0,filelen);
 //	filele=
-Log("I am after get_ramdisk");
-	filelen=ramdisk_read((void *)DEFAULT_ENTRY,0,filelen);
-	Log("I am pass ramdisk_read in loader");
+//Log("I am after get_ramdisk");
+//	filelen=ramdisk_read((void *)DEFAULT_ENTRY,0,filelen);
+//	Log("I am pass ramdisk_read in loader");
 //	size_t de=DEFAULT_ENTRY;
 //	printf("%d\n",filelen);
 //	printf("%x\n",(size_t)DEFAULT_ENTRY-ramdisk_start);
 //	printf("%d\n",(size_t)de-ramdisk_start);
+	int fd=fs_open(filename,0,0);
+	fs_read(fd,(void*)DEFAULT_ENTRY,fs_filesz(fd));
+	fs_close(fd);
+
+ 
+ 
   return DEFAULT_ENTRY;
 }
 

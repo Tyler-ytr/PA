6d636f9079edf6994d70a0bcaf39900351372416
tracer-ics2018 <tracer@njuics.org>
2018-11-30 22:00:33 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:00:33 up 12:08, 19 users,  load average: 0.34, 0.23, 0.19 1575cb42ec86e653b8dfd98ea750f4c664ae6fc0

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,5 @@
 #include "fs.h"
-#include <sys/types.h>
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -13,14 +13,6 @@ typedef struct {
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
-extern size_t ramdisk_read(void *buf,size_t offset,size_t len);
-extern size_t ramdisk_write(const void *buf,size_t offset,size_t len);
-int fs_open(const char*pathname,int flags,int mode);
-ssize_t fs_read(int fd,void *buf,size_t len);
-ssize_t fs_write(int fd,const void *buf,size_t len);
-size_t fs_filesz(int fd);
-off_t fs_lseek(int fd,off_t offset,int whence);
-int fs_close(int fd);
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -34,9 +26,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0,0, invalid_read, invalid_write},
+  {"stdout", 0, 0,0, invalid_read, invalid_write},
+  {"stderr", 0, 0,0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -44,78 +36,4 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-}
-
-
-int fs_open(const char*pathname,int flags,int mode)
-{Log("I am in open");
-	for(int i=0;i<NR_FILES;i++)
-	{
-	Log("filename:%s",file_table[i].name);
-	if(strcmp(file_table[i].name,pathname)==0)
-		return i;
-	}
-Log("fs_open GG");
-assert(0);
-return -1;
-}
-
-ssize_t fs_read(int fd,void *buf,size_t len)
-{Log("I am in read");
-	ssize_t fsize=fs_filesz(fd);
-	switch(fd){
-		case FD_STDOUT:Log("in case stdout of fs_read");assert(0);
-		case FD_STDERR:Log("in case stderr of fs_read");assert(0);
-		case FD_FB:Log("in case fd_fb of fs_read");assert(0);
-		default:{
-					if(file_table[fd].open_offset>fsize)
-						return 0;
-					if(file_table[fd].open_offset+len>fsize)
-				    {
-						len=fsize-file_table[fd].open_offset;
-					}
-				   ramdisk_read(buf,file_table[fd].open_offset+file_table[fd].disk_offset,len);
-				}
-	}
-	file_table[fd].open_offset+=len;
-	return len;
-}
-
-ssize_t fs_write(int fd,const void *buf,size_t len)
-{Log("I am in write");
-	ssize_t fsize=fs_filesz(fd);
-	switch(fd){
-		case FD_STDOUT:Log("in case stdout of fs_write");assert(0);
-		case FD_STDERR:Log("in case stderr of fs_write");assert(0);
-		case FD_FB:Log("in case fd_fb of fs_write");assert(0);
-		default:{
-					if(file_table[fd].open_offset>fsize)
-						return 0;
-					if(file_table[fd].open_offset+len>fsize)
-					{
-						len=fsize-file_table[fd].open_offset;
-					}
-					ramdisk_write(buf,file_table[fd].open_offset+file_table[fd].disk_offset,len);	
-				}
-	}
-	file_table[fd].open_offset+=len;
-	return len;
-}
-
-
-off_t fs_lseek(int fd,off_t offset,int whence)
-{
-	switch(whence){
-		case SEEK_SET:file_table[fd].open_offset=offset;break;
-		case SEEK_CUR:file_table[fd].open_offset+=offset;break;
-		case SEEK_END:file_table[fd].open_offset=file_table[fd].size+offset;break;
-		default:return -1;
-	}
-	if(file_table[fd].open_offset>file_table[fd].size)
-	{file_table[fd].open_offset=file_table[fd].size;
-	}return file_table[fd].open_offset;
-}
-size_t fs_filesz(int fd)
-{
-return file_table[fd].size;
 }

3dfc0b5cc7dfc57ef24435fdc8263dde41d83e06
tracer-ics2018 <tracer@njuics.org>
2018-12-04 21:24:09 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:24:09 up  2:48,  9 users,  load average: 0.15, 0.09, 0.07 9b93a1a1fd72d7f7ad13e1b58c916b094a506906

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,7 +30,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	int my_x,my_y,my_len;
+	offset=offset>>2;
+	my_x=offset%screen_width();	
+	my_y=offset/screen_width();
+	my_len=len>>2;
+	Log("offset:%d len:%d x:%d y:%d",offset,my_len,my_x,my_y);
+	draw_rect((uint32_t*)buf,my_x,my_y,my_len,1);
+  	return len;
 }
 
 void init_device() {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -47,7 +47,7 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[FD_FB].size=screen_height()*screen_width();
+  file_table[FD_FB].size=screen_height()*screen_width()*sizeof(size_t);
 }
 
 

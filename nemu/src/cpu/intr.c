8c47cb999d74a8f8b18b29f7a6b7be664d41067f
tracer-ics2018 <tracer@njuics.org>
2018-12-05 21:52:12 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:52:12 up 11:38, 16 users,  load average: 0.29, 0.18, 0.18 59566ec94cd7c1a59177363b3869300a20a3debf

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,18 +7,29 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
  //TODO();
+	
+
 	rtl_push((rtlreg_t *)&cpu.eflags);
 	rtl_push((rtlreg_t *)&cpu.cs);
 	rtl_push((rtlreg_t *)&ret_addr);
 
 	uint32_t idtr_base=cpu.idtr.base;
-	uint32_t eip_low,eip_high,offset;
+//	uint32_t eip_low,eip_high,offset;
+	uint32_t address=NO*8+idtr_base;
 
-	eip_low=vaddr_read(idtr_base+NO*8,4)&0x0000ffff;
-	eip_high=vaddr_read(idtr_base+NO*8+4,4)&0xffff0000;
-	offset=eip_low|eip_high;
-//	decoding.jmp_eip=offset;
+	uint32_t offset=vaddr_read(address+4,4);
+	uint32_t selector=vaddr_read(address,4);
+	offset=(offset&0xffff0000)+(selector&0xffff);
+	
 	rtl_j(offset);
+
+
+
+//	eip_low=vaddr_read(idtr_base+NO*8,4)&0x0000ffff;
+//	eip_high=vaddr_read(idtr_base+NO*8+4,4)&0xffff0000;
+//	offset=eip_low|eip_high;
+//	decoding.jmp_eip=offset;
+//	rtl_j(offset);
 //	decoding.jmp_eip=offset;
 //	decoding.is_jmp=true;
 

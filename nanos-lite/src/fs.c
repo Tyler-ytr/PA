88b427878c383f679883078de5cf7eb9164d655c
tracer-ics2018 <tracer@njuics.org>
2018-12-05 17:41:19 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:41:19 up  8:28, 16 users,  load average: 0.94, 0.87, 0.55 f684d02a23b2c8ea3e56edd3303d2e7beb40966

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -116,11 +116,11 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 	}
 	file_table[fd].open_offset+=len;
 	return len;*/
-	ssize_t size,newlen;
+//	ssize_t size,newlen;
 //	size=file_table[fd].size-file_table[fd].open_offset;
- size=fs_filesz(fd);
-					if(file_table[fd].open_offset>size)
-						return 0;
+ //size=fs_filesz(fd);
+			//		if(file_table[fd].open_offset>size)
+			//			return 0;
 		//			if(file_table[fd].open_offset+len>size)
 		//		    {
 		//				len=size-file_table[fd].open_offset;
@@ -130,7 +130,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 assert(0<=len+file_table[fd].open_offset);
 assert(len+file_table[fd].open_offset<=file_table[fd].disk_offset+file_table[fd].size);
 //	newlen=len>size?size:len;
-	newlen=len;		
+ssize_t	newlen=len;		
 	if(file_table[fd].read!=NULL)
 			{
 				printf("len:%d\n",len);

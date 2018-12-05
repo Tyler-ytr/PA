5c6b1d5a0ac6eb39f52e2e2651032cb5be1b0740
tracer-ics2018 <tracer@njuics.org>
2018-12-05 17:40:03 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:40:03 up  8:26, 16 users,  load average: 1.48, 0.92, 0.53 92a8945b3957bd82730e1d2bb9f94b2b47d259b6

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -125,11 +125,12 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 		//		    {
 		//				len=size-file_table[fd].open_offset;
 			//		}
-	//		if(len+file_table[fd].open_offset>file_table[fd].disk_offset+file_table[fd].size)
-	//	len=file_table[fd].size+file_table[fd].disk_offset-file_table[fd].open_offset;
-//assert(0<=len+file_table[fd].open_offset);
-//assert(len+file_table[fd].open_offset<=file_table[fd].disk_offset+file_table[fd].size);
-	newlen=len>size?size:len;
+			if(len+file_table[fd].open_offset>file_table[fd].disk_offset+file_table[fd].size)
+		len=file_table[fd].size+file_table[fd].disk_offset-file_table[fd].open_offset;
+assert(0<=len+file_table[fd].open_offset);
+assert(len+file_table[fd].open_offset<=file_table[fd].disk_offset+file_table[fd].size);
+//	newlen=len>size?size:len;
+	newlen=len;		
 	if(file_table[fd].read!=NULL)
 			{
 				printf("len:%d\n",len);

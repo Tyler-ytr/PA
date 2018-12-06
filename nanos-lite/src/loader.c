8520ad2ec52020b796ea37f7dcf1e54825d53d69
tracer-ics2018 <tracer@njuics.org>
2018-12-07 03:07:20 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  03:07:20 up 38 min,  8 users,  load average: 0.12, 0.23, 0.25 35c0d3c3d6dc7d87d10e9f11d7a43fa6e48589d6

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -35,7 +35,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-Log("I am in naive_uload");
+Log("I am in naive_uload: %s",filename);
   	uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
 }

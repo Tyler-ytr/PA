dc3df78b5a310357838dda45e91cca1658273991
tracer-ics2018 <tracer@njuics.org>
2018-12-07 14:30:24 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:30:24 up  1:12, 10 users,  load average: 0.32, 0.40, 0.39 e6d3100bda6b2628a52deeb99b5c5756712087c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -31,12 +31,13 @@ printf("a3(edx): 0x%x\n",a[3]);*/
   switch (a[0]) {
 	  case SYS_exit://_halt(a[1]);	
 					//
-					//naive_uload(NULL,"/bin/init");
-					c->GPRx=SYS_execve;
-					c->GPR2=(uintptr_t)"/bin/init"
+					naive_uload(NULL,"/bin/init");
+			//		c->GPRx=SYS_execve;
+			//		c->GPR2=(uintptr_t)"/bin/init"
+						
 						;
 					Log("I am in exit");
-					do_syscall(c);
+				//	do_syscall(c);
 					break;
 		  //ID=0 it should be a[1] and the result of that is I can't go through the dummy. the result of it is ebx=0x1.it has fixed.
 	  case SYS_yield:{c->GPRx=sys_yield();
@@ -92,6 +93,7 @@ printf("a3(edx): 0x%x\n",a[3]);*/
 //						Log("wait for sysexecve");assert(0);
 						Log("sys_execve: name=%s",(char*)a[1]);
 						naive_uload(NULL,(char*)a[1]);
+						c->GPRx=0;
 						break;
 						}
 		case SYS_fork:Log("wait for sysfork");assert(0);

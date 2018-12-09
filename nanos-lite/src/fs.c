12fbfafa3d78c18ff060ba3eabf941f3d801f1c8
tracer-ics2018 <tracer@njuics.org>
2018-12-09 21:37:40 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:37:40 up  7:37, 32 users,  load average: 2.41, 1.60, 1.32 29786e075a86be22107da6f5d80f5b1a419a21f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -156,12 +156,12 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 	file_table[fd].open_offset+=newlen;
 	return newlen;*/
 	int ret=0;
-	Log("fs_read : fd=%d , name=%s, offset=%d, len=%d ",fd,file_table[fd].name,file_table[fd].open_offset,len);
+//	Log("fs_read : fd=%d , name=%s, offset=%d, len=%d ",fd,file_table[fd].name,file_table[fd].open_offset,len);
 	if(fd!=FD_EVENTS&&file_table[fd].open_offset+len>file_table[fd].size)
 	{
 		len=file_table[fd].size-file_table[fd].open_offset;
 	}
-	Log("fs_read : len=%d",len);
+//	Log("fs_read : len=%d",len);
 	if(file_table[fd].read!=NULL)
 	{
 	ret=file_table[fd].read(buf,file_table[fd].open_offset,len);	

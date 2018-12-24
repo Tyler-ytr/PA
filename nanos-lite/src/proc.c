5783365f123075563f8951d1efdd2cb81c300db2
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:03:00 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:03:00 up 10:54, 35 users,  load average: 0.10, 0.11, 0.20 6a9d4ba5617ef698c91ead0291a1106f2e5c8ab2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,10 +22,10 @@ void hello_fun(void *arg) {
 void init_proc() {
 //	Log("I am in proc");
 //naive_uload(NULL,"/bin/init");
-//context_uload(&pcb[1],"/bin/pal");
+context_uload(&pcb[1],"/bin/pal");
 //context_uload(&pcb[1],"/bin/dummy");
-  context_uload(&pcb[1], "/bin/init");
-// context_kload(&pcb[0], (void *)hello_fun);  
+//  context_uload(&pcb[1], "/bin/init");
+ context_kload(&pcb[0], (void *)hello_fun);  
     switch_boot_pcb();
 }
 

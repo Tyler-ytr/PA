f6b32e74f011619f1b65b8836ee24aeacfb0b7fc
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:56:35 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:56:35 up 11:48, 35 users,  load average: 1.22, 0.55, 0.33 d1b85aa52e3d27da57a5c9798afc8ad0532356c7

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void init_proc() {
 //	Log("I am in proc");
 //naive_uload(NULL,"/bin/init");
-context_uload(&pcb[0],"/bin/pal");
+//context_uload(&pcb[0],"/bin/pal");
 context_uload(&pcb[1],"/bin/hello");
 //context_uload(&pcb[1],"/bin/dummy");
 //  context_uload(&pcb[1], "/bin/init");
@@ -49,8 +49,8 @@ printf("prev->eflags:0x%x\n",prev->eflags);*/
   current->cp = prev;
  
   // always select pcb[0] as the new process
-// current = &pcb[1];
-current = (current == &pcb[0] ? &pcb[1] : &pcb[0]); 
+ current = &pcb[1];
+//current = (current == &pcb[0] ? &pcb[1] : &pcb[0]); 
   // then return the new context
   return current->cp; 
 	

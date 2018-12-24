ae6c2cbaa4c8ec3ec0eda23a1f3187ec34d1571e
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:04:52 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:04:52 up 10:56, 35 users,  load average: 0.42, 0.20, 0.22 e07fe0498fa55a808d3f6259241a459d19fafded

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -48,8 +48,8 @@ printf("prev->eflags:0x%x\n",prev->eflags);*/
   current->cp = prev;
  
   // always select pcb[0] as the new process
-  current = &pcb[0];
-//current = (current == &pcb[0] ? &pcb[1] : &pcb[0]); 
+  current = &pcb[1];
+current = (current == &pcb[0] ? &pcb[1] : &pcb[0]); 
   // then return the new context
   return current->cp; 
 	

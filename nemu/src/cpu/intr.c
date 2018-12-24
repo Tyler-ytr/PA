5f04887701abac1fd478b013453c6949a79b70fc
tracer-ics2018 <tracer@njuics.org>
2018-12-24 21:29:45 +0800

    >  compile 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:29:45 up 52 min,  9 users,  load average: 0.10, 0.20, 0.22 9f15b199eea823f28831444135c19af5871b03d6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
- //TODO();
+ //TODO()
+	Log("NO:%d",NO);
 	
 
 	rtl_push((rtlreg_t *)&cpu.eflags);

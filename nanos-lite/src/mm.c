225ce6970869e468615f7a1d5429038da39efa87
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:18:14 +0800

    >  gdb 171240565 ytr Linux larrylucifer 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:18:14 up 11:10, 35 users,  load average: 0.84, 0.80, 0.55 c6cf07f035627e371799e424c23e009be58907e0

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,10 +16,10 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
-	if(current->cur_brk==0)	{
-		current->cur_brk = current->max_brk = new_brk;
-	}
-	else{
+//	if(current->cur_brk==0)	{
+//		current->cur_brk = current->max_brk = new_brk;
+//	}
+//	else{
 		if(new_brk>current->max_brk){
 			void* pgstart=(void*)PGROUNDUP(current->max_brk);
 			void* pgend=(void*)PGROUNDUP(new_brk);//from memory.h
@@ -35,7 +35,7 @@ int mm_brk(uintptr_t new_brk) {
 //			printf("max_after:0x%x\n",current->max_brk);
 		}
 		current->cur_brk=new_brk;		
-	}
+//	}
   return 0;
 }
 //目前初始化MM的工作有两项, 第一项工作是将TRM提供的堆区起始地址作为空闲物理页的首地址, 将来会通过new_page()函数来分配空闲的物理页. 为了简化实现, MM中采用顺序的方式对物理页进行分配, 而且分配后无需回收. 第二项工作是调用AM的_vme_init()函数, 填写内核的页目录和页表, 然后设置CR3寄存器, 最后通过设置CR0寄存器来开启分页机制
